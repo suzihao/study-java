9ce50c57df642db74530ece98a1a3671b16b8152
suhezihao <562763562@qq.com>
2020-04-27 11:38:00 +0800

    33

---
+++ src/com/company/leetCode/LeetCode33.java
@@ -0,0 +1,38 @@
+package com.company.leetCode;
+
+import java.util.Arrays;
+
+/**
+ * @author 苏和
+ * @version 2020/4/27
+ */
+public class LeetCode33 {
+
+    public static int search(int[] nums, int target) {
+        int len = nums.length;
+        int left = 0, right = len-1;
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < nums[right]){
+                if(nums[mid] < target && target <= nums[right])
+                    left = mid+1;
+                else
+                    right = mid-1;
+            }
+            else{
+                if(nums[left] <= target && target < nums[mid])
+                    right = mid-1;
+                else
+                    left = mid+1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        System.out.println(search(nums,0));
+    }
+}
