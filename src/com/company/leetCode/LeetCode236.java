c858e527f57f002b478107346f77556a8a4fe0dd
suzihao <562763562@qq.com>
2020-07-03 09:26:05 +0800

    Socket demo

---
+++ src/com/company/leetCode/LeetCode236.java
@@ -0,0 +1,22 @@
+package com.company.leetCode;
+
+public class LeetCode236 {
+
+    public static class TreeNode {
+        int val;
+        LeetCode199.TreeNode left;
+        LeetCode199.TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode res = new TreeNode(1);
+
+        return res;
+
+
+
+
+    }
+}
