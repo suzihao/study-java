0c311aa86d7d01cbb601c6a21f28e60ab328249a
suhe17 <suhe17@jd.com>
2025-05-14 15:32:42 +0800

    高德打车

---
+++ src/com/company/interview/AliInterview3.java
@@ -0,0 +1,12 @@
+package com.company.interview;
+
+public class AliInterview3 {
+
+    // 高德打车
+//    优惠券表：coupon (id, name, amount, inventory,version):
+//    用户优惠券表：user_coupon (id, user_id, coupon_id, coupon_name, coupon_amount):
+//
+//    领取优惠券时的事务实现：
+//    update coupon: invertory - 1;
+//    insert user_coupon
+}
