c4bb2215b8a1f36cebade930e4786bc49dad5c01
suhe17 <suhe17@jd.com>
2025-07-01 09:43:26 +0800

    每日一题

--- src/com/company/interview/AliInterview3.java
+++ src/com/company/interview/AliInterview3.java
@@ -1,5 +1,7 @@
 package com.company.interview;
 
+import java.util.Arrays;
+
 public class AliInterview3 {
 
     // 高德打车
@@ -9,4 +11,30 @@ public class AliInterview3 {
 //    领取优惠券时的事务实现：
 //    update coupon: invertory - 1;
 //    insert user_coupon
+
+
+    public static int[] maxSubsequence(int[] nums, int k) {
+        // 创建下标数组，对下标数组排序
+        Integer[] idx = new Integer[nums.length];
+        Arrays.setAll(idx, i -> i);
+        Arrays.sort(idx, (i, j) -> nums[j] - nums[i]);
+
+        // 对前 k 个下标排序
+        // 注：排序 int[] 比排序 Integer[] 快 2ms
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = idx[i];
+        }
+        Arrays.sort(ans);
+
+        // 取出 nums 的子序列
+        for (int i = 0; i < k; i++) {
+            ans[i] = nums[ans[i]];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(maxSubsequence(new int[]{2, 1, 3, 3}, 2)));
+    }
 }
