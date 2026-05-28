ee9aee0868514d240624880313b277818449fc07
suhe <562763562@qq.com>
2026-05-28 08:43:24 +0800

    运行调整

--- src/com/company/concurrent/c_026/T04_Executors.java
+++ src/com/company/concurrent/c_026/T04_Executors.java
@@ -8,7 +8,7 @@ import java.util.concurrent.Executors;
 public class T04_Executors {
 
     public static void main(String[] args) {
-        Executors executors = (Executors) Executors.newCachedThreadPool();
+//        Executors executors = (Executors) Executors.newCachedThreadPool();
         //...
     }
 
